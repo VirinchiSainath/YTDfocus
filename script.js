05c8aba6ba6ef90535744aac383f7711134697a9
vnalluri <vnalluri@iupui.edu>
2018-04-01 13:28:11 -0400

    basic running
    Added a running content script

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("content script about to run");
+var ytdBrowse = document.getElementsByTagName('ytd-browse');
+ytdBrowse[0].style.visibility="hidden";
+console.log("script run complete");
